92359fee3c7a50244fe45a4b3826aa64d89cac59
Reginaldo J. Santos <60084851+regisskate@users.noreply.github.com>
2025-06-11 23:30:48 -0300

    chore: component subscripition

--- src/app/(protected)/subscription/_components/subscription-plan.tsx
+++ src/app/(protected)/subscription/_components/subscription-plan.tsx
@@ -75,7 +75,7 @@ export function SubscriptionPlan({
           Para profissionais autônomos ou pequenas clínicas
         </p>
         <div className="flex items-baseline">
-          <span className="text-3xl font-bold text-gray-900">R$59</span>
+          <span className="text-3xl font-bold text-gray-900">R$60</span>
           <span className="ml-1 text-gray-600">/ mês</span>
         </div>
       </CardHeader>
